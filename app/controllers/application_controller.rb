321eaf55b1339226473e9fd6a4faed0eaa706c38
tomo <o_tomo_1990@yahoo.co.jp>
2023-02-12 10:29:41 +0900

    言語切替ボタンの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  before_action :switch_locale
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def switch_locale
+    I18n.locale = params[:locale] if params[:locale]
   end
 end
